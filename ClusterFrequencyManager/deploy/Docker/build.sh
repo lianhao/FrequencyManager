a443c0ff45d6669cc41c9d08e27b91b3f0bbe45c
Patrick Kutch <Patrick.Kutch@gmail>
2019-02-21 12:20:59 -0800

    Adding some instructions

--- ClusterFrequencyManager/deploy/Docker/build.sh
+++ ClusterFrequencyManager/deploy/Docker/build.sh
@@ -1,3 +1,3 @@
 cp ../../src/*.py .
-cp ../../../../grpcProtobuff/*.proto .
+cp ../../../grpcProtobuff/*.proto .
 docker build --no-cache --build-arg http_proxy=$http_proxy --build-arg https_proxy=$http_proxy --build-arg HTTP_PROXY=$http_proxy --build-arg HTTPS_PROXY=$http_proxy --network=host -t patrickkutch/clusterfrequencymanager .
